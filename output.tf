12703042a9630dbb96d4ada43b28ca87a990c0ee
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-07-24 13:25:11 +0930

    fix named outputs

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "function_arns" {
   value = {
-    for func_name, function in aws_lambda_function.edge_functions :
-    func_name => function.arn
+    for function in aws_lambda_function.edge_functions :
+    function.function_name => function.arn
   }
 }
  
