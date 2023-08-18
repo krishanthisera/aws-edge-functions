94c0cf623333eadba9995ca2b2359d1882102ee7
Krishan Thisera <krishan.thisera@aligent.com.au>
2023-08-18 21:37:20 +0930

    refine directory structure

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 locals {
   edge_functions = [
     {
-      name = "prerender"
-      path = "${var.edge_function_path}/packages/prerender/build/index.js"
+      name = "prerender-proxy"
+      path = "${var.edge_function_path}/packages/prerender-proxy/build/index.js"
       handler = "index.handler"
     },
     {
-      name = "prerender-check"
-      path = "${var.edge_function_path}/packages/prerender-check/build/index.js"
+      name = "filter-function"
+      path = "${var.edge_function_path}/packages/filter-function/build/index.js"
       handler = "index.handler"
     },
     {
-      name = "cache-control"
-      path = "${var.edge_function_path}/packages/cache-control/build/index.js"
+      name = "response-handler"
+      path = "${var.edge_function_path}/packages/response-handler/build/index.js"
       handler = "index.handler"
     }
   ]
